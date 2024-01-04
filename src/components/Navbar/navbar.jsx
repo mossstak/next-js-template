31b0cc2305d767e340541dc7b1ae709837db79bd
Mostak Khan <Mostak1993@gmail.com>
2024-01-04 22:05:28 +0000

    added files

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link"
+
+const navbar = () => {
+  return (
+    <div>
+    <Link href={"./About"}>About</Link>
+    </div>
+  )
+}
+
+export default navbar
